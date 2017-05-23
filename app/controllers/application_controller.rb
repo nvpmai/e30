ca0821d9fe73435d3bece31466de2106402414bd
nvpmai95 <nvpmai95@gmail.com>
2017-05-23 12:03:01 +0700

    show article

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,4 @@ class ApplicationController < ActionController::Base
   def require_login
     redirect_to '/' unless current_user
   end
-
-  def liked_ids
-    if current_user
-      @liked_ids = current_user.articles.pluck(:id)
-    end
-  end
 end
