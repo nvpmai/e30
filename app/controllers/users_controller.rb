d82b10f76fdce2d67f46ea228fd449ae954b9166
nvpmai95 <nvpmai95@gmail.com>
2017-05-23 21:37:37 +0700

    fix duplicate email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,7 @@ class UsersController < ApplicationController
 		user = User.new(user_params)
 	  if user.save
 	    session[:user_id] = user.id
-	    redirect_to '/'
-    else
-    	redirect_to '/'
+	    redirect_to root_path
 	  end
 	end
 
